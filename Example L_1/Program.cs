fa3b03cb744f8581ce1b4ef9e0ebc04f2107a1b2
PaulFritz <knjazkov.pavel@mail.ru>
2022-09-01 19:06:36 +0300

    добавил пример с машей

--- Example L_1/Program.cs
+++ Example L_1/Program.cs
@@ -1,22 +1,53 @@
 ﻿// Первая программа из 1 лекции СЛОЖЕНИЕ ЧИСЕЛ
+
 // int numberA = 28;
 // int numberB = 49;
 // Console.WriteLine(numberA + numberB);
 
+
 // Она же но через result
+
 // int numberA = 28;
 // int numberB = 49;
 // int result = numberA + numberB;
 // Console.WriteLine(result);
 
+
 // Вторая программа из 1 лекции ДЕЛЕНИЕ
+
 // int numberA = 28;
 // int numberB = 4;
 // int result = numberA / numberB;
 // Console.WriteLine(result);
 
+
 // Вторая программа из 1 лекции ДЕЛЕНИЕ но вещественные числа
-double numberA = 28;
-double numberB = 46;
-double result = numberA / numberB;
-Console.WriteLine(result);
+
+// double numberA = 28;
+// double numberB = 46;
+// double result = numberA / numberB;
+// Console.WriteLine(result);
+
+
+//Третья программа из 1 лекции
+
+// int numberA = new Random().Next(1,10); // 123...9
+// Console.WriteLine (numberA);
+// int numberB = new Random().Next(1,10);
+// Console.WriteLine (numberB);
+// Console.WriteLine(numberA + numberB);
+
+
+//Четвертая программа из 1 лекции ИМЯ
+
+Console.Write ("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username == "Маша")
+{
+    Console.WriteLine("Ура Маша!))");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
