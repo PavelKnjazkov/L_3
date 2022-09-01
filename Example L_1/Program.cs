8ce6ae1445651bc7a9598cf15efcdb79d4e065a4
PaulFritz <knjazkov.pavel@mail.ru>
2022-09-01 15:14:09 +0300

    add random

---
+++ Example L_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Первая программа из 1 лекции СЛОЖЕНИЕ ЧИСЕЛ
+// int numberA = 28;
+// int numberB = 49;
+// Console.WriteLine(numberA + numberB);
+
+// Она же но через result
+// int numberA = 28;
+// int numberB = 49;
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
+// Вторая программа из 1 лекции ДЕЛЕНИЕ
+// int numberA = 28;
+// int numberB = 4;
+// int result = numberA / numberB;
+// Console.WriteLine(result);
+
+// Вторая программа из 1 лекции ДЕЛЕНИЕ но вещественные числа
+double numberA = 28;
+double numberB = 46;
+double result = numberA / numberB;
+Console.WriteLine(result);
--- L_3_1_1/Program.cs
+++
@@ -1,2 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ну ты и олень!");
