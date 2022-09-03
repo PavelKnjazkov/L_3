c7f6ea451e31a40690333d8f1cc1401ef0df7b53
PaulFritz <knjazkov.pavel@mail.ru>
2022-09-04 00:24:49 +0300

    Решил первые 2-ве задачи ДЗ 1-ой лекции в папке Homework

--- Homework _1/Program.cs
+++ Homework _1/Program.cs
@@ -1,16 +1,76 @@
 ﻿//       !!!ДОМАШНЯЯ РАБОТА!!!
+
 // Урок 1. Знакомство с языком программирования С#
+
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+//РЕШЕНИЕ:
+
+// первый вариант с произвольным числом от 1 до 1000
+
+// Console.WriteLine ("Сейчас я загадаю два числа и покажу тебе какое из них будет или больше, или меньше!:))");
+// int numberA = new Random().Next(1,100); // 123... 1000
+// Console.WriteLine ("Моим первым число будет: " + numberA);
+// int numberB = new Random().Next(1,100); // 123... 1000
+// Console.WriteLine ("А на второе я выбрал: " + numberB);
+// int max = numberA;
+// int min = numberB;
+// if (numberA > max ) max = numberA; 
+// if (numberB > max ) max = numberB;
+// if (numberA < min ) min = numberA; 
+// if (numberB < min ) min = numberB;
+// Console.WriteLine ("Большим будет - " + max);
+// Console.WriteLine ("Ну а поменьше - " + min);
+// Console.WriteLine ("А тебе так слабо!?");
+
+// второй вариант ввод чисел производит пользователь
+
+// int a, b;
+// Console.WriteLine("А сейчас мы определим какое из двух чисел больше!");
+// Console.WriteLine("Для этого введите первое число: ");
+// a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("И ещЁ одно число: ");
+// b = Convert.ToInt32(Console.ReadLine());
+// if (a > b)
+// {
+// Console.WriteLine("{0} - наибольшее число!", a);
+// Console.WriteLine("{0} - наименьшее число!", b);
+// }
+// else
+// {
+// Console.WriteLine("{0} - наибольшее число!", b);
+// Console.WriteLine("{0} - наименьшее число!", a);
+// }
+
+
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+// int a, b, c;
+
+// Console.WriteLine("На этот раз мы определим какое из трех чисел больше!");
+// Console.Write("Для этого введите первое число: ");
+// a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Теперь второе число: ");
+// b = Convert.ToInt32(Console.ReadLine());
+// Console.Write("И наконец третье число: ");
+// c = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+
+// if (a > max ) max = a;
+// if (b > max ) max = b;
+// if (c > max ) max = c;
+
+// Console.WriteLine("А вот и все наши числа: " + a + ", "  + b + ", " + c + ", " + "где большим будет: " + max);
+// Console.WriteLine("Ура!!!");
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
