8420441c0619c05a2033c3402074139ecadb5904
PaulFritz <knjazkov.pavel@mail.ru>
2022-09-03 20:47:34 +0300

    Добавил все примеры из 1-ой лекции в Example L_1

--- Example L_1/Program.cs
+++ Example L_1/Program.cs
@@ -70,7 +70,7 @@
 // }
 
 
-    // 5-ая программа - приер с гирями (найти наибольшее число)
+    // 5/6-ая программа - приер с гирями (найти наибольшее число)
 
 // int a = 1;
 // int b = 2;
@@ -89,4 +89,51 @@
 // Console.Write("max = ");
 // Console.WriteLine(max);
 
-END
+    // 7-ая программа - Magic
+//Запоминаем команды:
+Console.Clear();
+// Console.SetCursorPosition(10, 4);
+// Console.WriteLine("+");
+
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 0;
+
+while(count < 10000)
+
+{
+    int what = new Random().Next(0, 3); // 1 2 3
+    if (what ==0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what ==1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what ==2)
+    {
+        x = (x + xc) / 2;
+        y = (y + yc) / 2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count = count + 1; // или count +=1; или count++;
+}
+
+//        !!!END!!!
