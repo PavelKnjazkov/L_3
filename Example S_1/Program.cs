3d876acf15d1b384d940dab6a5f1a7d0bf2c60ed
PaulFritz <knjazkov.pavel@mail.ru>
2022-09-04 15:46:30 +0300

    Что то не так с сохранением в основной папке

--- Example S_1/Program.cs
+++ Example S_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+// Например:
+// 4 -> 16 
+// -3 -> 9 
+// -7 -> 49
+
+//РЕШЕНИЕ:
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+//получение квадрата входного числа
+int result = number * number; 
+Console.WriteLine($"Квадрат числа {number} = {result}");
+
+//получение квадрата входного числа математической функцией
+int result_2 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа {number} = {result_2}");
