53e6a001a06476556b14893e9d0fc25cdfb99a7b
PaulFritz <knjazkov.pavel@mail.ru>
2022-09-09 19:36:45 +0300

    Сделал задачу с 2 точками в 3D

--- Homework_3/HW_21 2 poits in 3D/Program.cs
+++ Homework_3/HW_21 2 poits in 3D/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+// Подход: формула для расстояния между двумя точками в 3-х измерениях, 
+// т.е. (x1, y1, z1) и (x2, y2, z2) была получена из теоремы Пифагора, которая такова:
+// Расстояние = $ \ sqrt {(x2-x1) ^{2} + (y2-y1) ^{2} + (z2-z1) ^{2}} $ 
+
+
+Console.WriteLine("Enter coordinates point А: ");
+Console.WriteLine("Enter x: ");
+int Ax = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter y: ");
+int Ay = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter z: ");
+int Az = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter coordinates point B: ");
+Console.WriteLine("Enter x: ");
+int Bx = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter y: ");
+int By = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter z: ");
+int Bz = Convert.ToInt32(Console.ReadLine());
+
+double Decision = Math.Sqrt (Math.Pow(Bx - Ax, 2) + Math.Pow(By - Ay, 2) + Math.Pow(Bz - Az, 2));
+Console.WriteLine($"Длина отрезка  {Decision}");
+
