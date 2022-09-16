287846f5920b929bda490c391424411582a398c3
PaulFritz <knjazkov.pavel@mail.ru>
2022-09-16 18:15:00 +0300

    Добавил отдельными папками задачи 4-го семинара

--- Семинары/Example S_4/Program.cs
+++ Семинары/Example S_4/Program.cs
@@ -66,26 +66,3 @@
 // int a = int.Parse(Console.ReadLine());
 // int mult = GetMult(a);
 // Console.WriteLine(mult);
-
-// Задача 30: Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.
-// [1,0,1,1,0,1,0,0]
-
-void GetArray(int[] arr)
-{
-    int length = arr.Length;
-    for (int i = 0; i < length; i++)
-    {
-        arr[i] = new Random().Next(0, 2);
-    }
-}
-void PrintArray(int[] arr)
-{
-    int length = arr.Length;
-    for (int i = 0; i < length; i++)
-    {
-        Console.Write($"{arr[i]},");
-    }
-}
-int[] array = new int[8];
-GetArray(array);
-PrintArray(array);
