024fa3ebc4d82a35dc09396cfddff4b7c740c1fc
PaulFritz <knjazkov.pavel@mail.ru>
2022-09-16 11:10:37 +0300

    Доделал ДЗ в папке Homework_4

--- Homework/Homework_4/HW_25 Exponentiation/Program.cs
+++ Homework/Homework_4/HW_25 Exponentiation/Program.cs
@@ -1,3 +1,46 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Решить задачу с использованием методов.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
+// РЕШЕНИЕ 1 не через метод)))
+// Почему далее ругается и выводит только через double.Parse!?
+
+// int x = 3;
+// int stepen = 5;
+// Console.WriteLine("3^5 = " + Math.Pow(x, stepen));
+
+// РЕШЕНИЕ 2 МЕТОД ЧЕРЕЗ MATH
+
+double Exponentiation(double numberA, double numberB) // название метода: возведение в степень - exponentiation
+{
+    double result = Math.Pow(numberA, numberB); // мат формула возведения в степень
+    return result;
+}
+// ввод числа и степени пользователем и считывание их в переменные duoble = double.Parse
+Console.Write("Введите число которое хотите возвести в степень: ");
+double numberA = double.Parse(Console.ReadLine());
+Console.Write("Введите степень для возведения вашего числа: ");
+double numberB = double.Parse(Console.ReadLine());
+
+//  тут я не понимаю что тут происходит и зачем result - как сюда попадают данные из result!!!
+double exponentiation = Exponentiation(numberA, numberB);
+Console.WriteLine("Вот результат введения вашего числа в обозначенную степень: " + exponentiation);
+
+// РЕШЕНИЕ ЦИКЛОМ
+
+// int Exponentiation(int numberA, int numberB)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= numberB; i++)
+//     {
+//         result = result * numberA;
+//     }
+//     return result;
+// }
+// Console.Write("Введите число A: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число B: ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+
+// int exponentiation = Exponentiation(numberA, numberB);
+// Console.WriteLine("Ответ: " + exponentiation);
--- Homework/Homework_4/HW_27 SumNumber/Program.cs
+++ Homework/Homework_4/HW_27 SumNumber/Program.cs
@@ -3,4 +3,37 @@
 // 82 -> 10
 // 9012 -> 12
 
+// РЕШЕНИЕ: 
 
+// Console.Write("Введите число N: ");
+// int numberN = Convert.ToInt32(Console.ReadLine());
+
+// int SumNumber(int numberN)
+// {
+//     int counter = Convert.ToString(numberN).Length;
+//     int advance = 0;
+//     int result = 0;
+//     for (int i = 0; i < counter; i++)
+//     {
+//         advance = numberN - numberN % 10;
+//         result = result + (numberN - advance);
+//         numberN = numberN / 10;
+//     }
+//     return result;
+// }
+// int sumNumber = SumNumber(numberN);
+// Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+// РЕШЕНИЕ:
+
+Console.WriteLine("Введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0; // 0 - для sum - старт для счетцика!
+
+while (i > 0) // пока введенное число i > 0 будет происходить операция ниже!  
+{
+int num = i % 10; // остаток при делении получили и записали в num! 351%10=1 35%10=5 3%10=3
+i = i / 10; // целочисленный int при делении отбросил последнюю часть! 351/10=35
+sum = sum + num; // ЦИКЛ тут к 0 прибавляем остаток от деления !))) и получаем итоговую сумму пока i>0 цикл 1+5+3=9
+}
+Console.WriteLine("Сумма всех цифр в числе равна: " + sum); // вывод значения на экран!
