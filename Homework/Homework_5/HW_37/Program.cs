40d6b876ac1f7fe4ece549f55de6da7ef9efce79
PaulFritz <knjazkov.pavel@mail.ru>
2022-09-16 20:52:23 +0300

    Создал папку с упр 6 семинара

---
+++ Homework/Homework_5/HW_37/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+// Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] GetPairMult(int[] array)
+{
+    int size = 0;
+    int[] result = null;
+    if (array.Length % 2 == 0)
+    {
+        size = array.Length / 2;
+        result = new int[size];
+
+        for (int i = 0; i < size; i++) ;
+        {
+            result[i] = array[i] * array[array.Length - i - 1];
+        }
+    }
+    else
+    {
+        size = (array.Length / 2) + 1;
+        result = new int[size];
+
+        for (int i = 0; i < size; i++) ;
+    }
+    int[] result = new int[size];
+
+    for (int i = 0; i < rusult.Length; i++)
+    {
+        if (i = 0; i < size; i++)
+    }
+    {
+        result[i] = array[i] * array[size - 1 - i];
+    }
+}
+
+int[] array = new int[] { 1, 4, 6, 3, 7 };
---
+++ Семинары/Example S_6/39/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, 
+// а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int [] GetReversArray1(int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = array[array.Length - 1 - i];
+    }
+
+    return result;
+}
+
+int[] array = new int[] {1, 2, 3, 4, 5};
+int[] result = GetReversArray1(array);
+
+Console.ReadKey();
