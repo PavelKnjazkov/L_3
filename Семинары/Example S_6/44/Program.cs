6c84cd1b4904ac497799cd1335ea8baaa2ac5806
PaulFritz <knjazkov.pavel@mail.ru>
2022-09-18 19:50:15 +0300

    добавил задачи семинара 6

--- Семинары/Example S_6/44/Program.cs
+++ Семинары/Example S_6/44/Program.cs
@@ -2,3 +2,32 @@
 // Если N = 5 -> 0 1 1 2 3
 // Если N = 3 -> 0 1 1
 // Если N = 7 -> 0 1 1 2 3 5 8
+
+int [] GetFibonacci (int size)
+{
+    int [] array = new int [size];
+
+    for (int i = 0; i < size; i++)
+    {
+        if (i == 0)
+        {
+            array[i] = 0;
+        }
+        else if (i == 1 || i == 2)
+        {
+            array[i] = 1;
+        }
+        else
+        {
+            array[i] = array [i - 2] + array [i - 1];
+        }
+    }
+        return array;
+}
+
+Console.WriteLine("Input number");
+int size = Convert.ToInt32(Console.ReadLine());
+
+int [] fibonacci = GetFibonacci(size);
+
+Console.WriteLine("[{0}]", string.Join(", ", fibonacci));
