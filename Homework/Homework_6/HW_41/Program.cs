97585c22a960af02c296cd781b70bcae58477cff
PaulFritz <knjazkov.pavel@mail.ru>
2022-09-22 21:23:39 +0300

    Homework_6 сделано

--- Homework/Homework_6/HW_41/Program.cs
+++ Homework/Homework_6/HW_41/Program.cs
@@ -1,1 +1,18 @@
-﻿Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// CountNumKeyboard
+
+Console.Clear();
+System.Console.WriteLine();
+int volume = 7;                // колчество ввода чисел
+int count = 0;
+
+while (volume > 0)
+{
+    System.Console.Write("Введите число:   ");
+    string num = Console.ReadLine();
+    int number = Convert.ToInt32(num);
+    if (number > 0) count++;
+    volume--;
+}
+
+System.Console.WriteLine($"Количество чисел больше 0: {count}");
