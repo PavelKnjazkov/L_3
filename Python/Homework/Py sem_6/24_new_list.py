3b7f7c7ca68ef5981eb91e359585e27385620b81
PaulFritz <knjazkov.pavel@mail.ru>
2022-12-15 17:18:20 +0300

    Дз all 6 сем

--- Python/Homework/Py sem_6/24_new_list.py
+++ Python/Homework/Py sem_6/24_new_list.py
@@ -2,8 +2,10 @@
 
 # [12,'sadf',5643] ---> ['sadf'] ,[12,5643]
 
+#Решение с семинара
+
 new_list = [12, 'sadf', 5643]
 list2 = list(filter(lambda x: type(x) == int, new_list))
 list3 = list(filter(lambda x: type(x) == str, new_list))
-print("Числа в списке:", *list3)
+print("Числа в списке:", *list2)
 print("Строки в списке:", *list3)
