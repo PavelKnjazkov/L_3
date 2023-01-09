95cb11dae946643b1614b657fbcc63f885022a64
PaulFritz <knjazkov.pavel@mail.ru>
2023-01-09 09:11:02 +0300

    Дз 9 сем глючит VSC

--- Python/Семинары/сем_9/бот телега.py
+++ Python/Семинары/сем_9/бот телега.py
@@ -1,59 +1,59 @@
-# from telegram import Bot
-# from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
-# from random import randint as rd
+from telegram import Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from random import randint as rd
 
-# #TOKEN = "5877452533:AAEogg4E_d6AplIEYtvfew_gmH1rmYlarEs"
+#TOKEN = "5877452533:AAEogg4E_d6AplIEYtvfew_gmH1rmYlarEs"
 
-# bot = Bot(token="5877452533:AAEogg4E_d6AplIEYtvfew_gmH1rmYlarEs")
-# updater = Updater(token="5877452533:AAEogg4E_d6AplIEYtvfew_gmH1rmYlarEs")
-# dispatcher = updater.dispatcher
-
-
-# A = 0
-# B = 1
+bot = Bot(token="5877452533:AAEogg4E_d6AplIEYtvfew_gmH1rmYlarEs")
+updater = Updater(token="5877452533:AAEogg4E_d6AplIEYtvfew_gmH1rmYlarEs")
+dispatcher = updater.dispatcher
 
 
-# def start(update, context):
-#     context.bot.send_message(update.effective_chat.id,"Привет, как у вас дела?")
-#     return A
+A = 0
+B = 1
 
-# def howareyou(update, context):
-#     text = update.message.text
-#     if 'хор' in text.lower():
-#         context.bot.send_message(update.effective_chat.id,"Я рад, что у тебя всё хорошо")
-#     else:
-#         context.bot.send_message(update.effective_chat.id,"Не грусти, всё будет отлично")
-#     context.bot.send_message(update.effective_chat.id,"Как погода?")
-#     return B
 
-# def weather(update, context):
-#     text = update.message.text
-#     context.bot.send_message(update.effective_chat.id,"И у меня такая же погода")
-#     return ConversationHandler.END
+def start(update, context):
+    context.bot.send_message(update.effective_chat.id,"Привет, как у вас дела?")
+    return A
 
-# def func(update, context):
-#     text = update.message.text
-#     if 'прив' in text.lower():
-#         context.bot.send_message(update.effective_chat.id,"И тебе привет, мой дорогой друг!")
-#     else:
-#         context.bot.send_message(update.effective_chat.id,"Я тебя не понимаю ;(")
+def howareyou(update, context):
+    text = update.message.text
+    if 'хор' in text.lower():
+        context.bot.send_message(update.effective_chat.id,"Я рад, что у тебя всё хорошо")
+    else:
+        context.bot.send_message(update.effective_chat.id,"Не грусти, всё будет отлично")
+    context.bot.send_message(update.effective_chat.id,"Как погода?")
+    return B
 
-# def cancel(update, context):
-#     context.bot.send_message(update.effective_chat.id,"Прощай")
+def weather(update, context):
+    text = update.message.text
+    context.bot.send_message(update.effective_chat.id,"И у меня такая же погода")
+    return ConversationHandler.END
 
-# start_handler = CommandHandler('start', start)
-# howareyou_handler = MessageHandler(Filters.text, howareyou)
-# weather_handler = MessageHandler(Filters.text, weather)
-# cancel_handler = CommandHandler('cancel', cancel)
+def func(update, context):
+    text = update.message.text
+    if 'прив' in text.lower():
+        context.bot.send_message(update.effective_chat.id,"И тебе привет, мой дорогой друг!")
+    else:
+        context.bot.send_message(update.effective_chat.id,"Я тебя не понимаю ;(")
 
-# conv_handler = ConversationHandler(entry_points=[start_handler],
-#                                     states={A: [howareyou_handler],
-#                                     B: [weather_handler]
-#                                     },
-#                                     fallbacks=[cancel_handler]
-#                                     )
+def cancel(update, context):
+    context.bot.send_message(update.effective_chat.id,"Прощай")
 
-# dispatcher.add_handler(conv_handler)
+start_handler = CommandHandler('start', start)
+howareyou_handler = MessageHandler(Filters.text, howareyou)
+weather_handler = MessageHandler(Filters.text, weather)
+cancel_handler = CommandHandler('cancel', cancel)
 
-# updater.start_polling()
-# updater.idle()
+conv_handler = ConversationHandler(entry_points=[start_handler],
+                                    states={A: [howareyou_handler],
+                                    B: [weather_handler]
+                                    },
+                                    fallbacks=[cancel_handler]
+                                    )
+
+dispatcher.add_handler(conv_handler)
+
+updater.start_polling()
+updater.idle()
