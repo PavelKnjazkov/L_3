0b5e9f3991b6045269f47b175ba8eb5dfddf365b
PaulFritz <knjazkov.pavel@mail.ru>
2023-01-09 18:21:53 +0300

    Дз 9 сем сохраняем, не работает VSC тупит мол не файла(

--- Python/Homework/Py sem_9/бот абв.py
+++ Python/Homework/Py sem_9/бот абв.py
@@ -21,16 +21,16 @@ def del_abv(update, context):
     context.bot.send_message(update.effective_chat.id, " ".join(list1))
 
 
-def del_abvV2(update, context):
-    text = update.message.text.split()
-    list1 = []
-    for i in text:
-        if "абв" in i:
-            list1.append(i)
-    context.bot.send_message(update.effective_chat.id, " ".join(list1[1:]))
+# def del_abvV2(update, context):
+#     text = update.message.text.split()
+#     list1 = []
+#     for i in text:
+#         if "абв" in i:
+#             list1.append(i)
+#     context.bot.send_message(update.effective_chat.id, " ".join(list1[1:]))
 
 
-hand_com = CommandHandler("filter", del_abvV2)
+# hand_com = CommandHandler("filter", del_abvV2)
 del_handler = MessageHandler(Filters.text, del_abv)
 dispatcher.add_handler(del_handler)
 dispatcher.add_handler(hand_com)
