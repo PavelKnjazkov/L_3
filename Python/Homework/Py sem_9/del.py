c5b1c79519fc28de9477922cad25d4f2fce18933
PaulFritz <knjazkov.pavel@mail.ru>
2023-01-12 19:44:13 +0300

    Добавил ДЗ 10 семинара

--- Python/Homework/Py sem_9/del.py
+++ Python/Homework/Py sem_9/del.py
@@ -1,15 +1,17 @@
+## наоборот удаляет все слова КРОМЕ слов с абв
+
 from telegram import Bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 from random import randint as rd
 
-bot = Bot(token="5877452533:AAEogg4E_d6AplIEYtvfew_gmH1rmYlarEs")
-updater = Updater(token="5877452533:AAEogg4E_d6AplIEYtvfew_gmH1rmYlarEs")
+bot = Bot(token=)
+updater = Updater(token=)
 dispatcher = updater.dispatcher
 
 def start(update, context):
      context.bot.send_message(update.effective_chat.id, 'Привет!\n\nЯ умею удалять из твоих сообщений слова, в которых есть сочетание "абв"')
      context.bot.send_message(update.effective_chat.id, 'Напиши мне любое сообщение, и я верну его обратно без слов с "абв"')
-     
+
 
 def del_abv(update, context):
     text = update.message.text.split()
